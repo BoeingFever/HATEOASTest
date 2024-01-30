e45e72f532be59ad2a164b6b1d7611b585e571ec
BoeingFever <h1355987@connect.hku.hk>
2024-01-30 22:28:35 +0800

    initial commit

---
+++ src/test/java/com/example/HATEOASTest/HateoasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HATEOASTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
