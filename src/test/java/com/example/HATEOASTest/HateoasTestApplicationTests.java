07da2634fe614965169e7f215c054b4348213939
BoeingFever <h1355987@connect.hku.hk>
2024-03-06 00:13:51 +0800

    Update HateoasTestApplicationTests.java

--- src/test/java/com/example/HATEOASTest/HateoasTestApplicationTests.java
+++ src/test/java/com/example/HATEOASTest/HateoasTestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.HATEOASTest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HateoasTestApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Autowired
+	private MockMvc mvc;
 
+	@Test
+	public void getTotalEmployees() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/employees").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 }
