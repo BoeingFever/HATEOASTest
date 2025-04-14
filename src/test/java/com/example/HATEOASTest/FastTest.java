9959245d4aa9d4d2f38fc05745734f98dbd8a096
BoeingFever <h1355987@connect.hku.hk>
2025-04-15 00:14:47 +0800

    added some tests and upgrade JDK and SpringBoot

--- src/test/java/com/example/HATEOASTest/FastTest.java
+++ src/test/java/com/example/HATEOASTest/FastTest.java
@@ -1,17 +1,36 @@
 package com.example.HATEOASTest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class FastTest {
     @Test
+    @DisplayName("Just assert true nothing else")
     public void fastTest() throws InterruptedException {
+        //Only if the assumeTrue method returns true the test will run
+        //Other assume xxx method works similarly
+        assumeTrue(false);
         assertTrue(true);
     }
 
     @Test
-    public void fiveSecondTest() throws InterruptedException {
-        Thread.sleep(5000);
+    @DisplayName("just thread sleep 2 sec, also try better visual name")
+    public void waitTest() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    public void addTest() throws InterruptedException {
+        BigDecimal expected = new BigDecimal(60);
+        BigDecimal result = new BigDecimal(20).add(new BigDecimal(30));
+        assertEquals(expected,result);
     }
 }
